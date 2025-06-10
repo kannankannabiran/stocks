d495d55b821cb87b9887d7e46162805a793b030e
kannankannabiran <kannankannabiran@gmail.com>
2025-06-11 03:29:46 +0530

    Alla Changes Done by kanann

--- src/ChartWithIndicators.js
+++ src/ChartWithIndicators.js
@@ -300,5 +300,5 @@ export default function ChartWithIndicators({ data, selectedIndicators }) {
     },
   };
 
-  return <Chart options={options} series={series} type="candlestick" height={700} />;
+  return <Chart options={options} series={series} type="candlestick" height={650} />;
 }
